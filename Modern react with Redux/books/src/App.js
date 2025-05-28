2b4ac5b5e4dc0cb979ec4ffe9f4d36f4e85ca945
Kieran <blacknoir_619@hotmail.co.uk>
2025-05-28 19:31:04 +0100

    completed all axios requests

--- Modern react with Redux/books/src/App.js
+++ Modern react with Redux/books/src/App.js
@@ -1,14 +1,25 @@
 import { useState } from "react";
+import axios from "axios";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import { useEffect } from "react";
 
 function App() {
   const [books, setBooks] = useState([]);
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
 
-  const editBookById = (id, newTitle) => {
+    setBooks(response.data);
+  };
+
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
@@ -16,7 +27,9 @@ function App() {
     setBooks(updatedBooks); // Close the editBookById function properly here
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById =  async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
@@ -24,18 +37,23 @@ function App() {
     setBooks(updatedBooks);
   };
 
-  const createBook = (title) => {
-    const updatedBooks = [
-      ...books,
-      { id: Math.round(Math.random() * 9999), title },
-    ];
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    const updatedBooks = [...books, response.data];
     setBooks(updatedBooks);
   };
 
   return (
     <div className="app">
       <h1>reading list</h1>
-      
+
       <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
